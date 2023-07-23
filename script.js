86ca27ee54e6c066e5592a820a6a8de78b854dd7
Natalia Domanska <nataliaidomanska@gmail.com>
2023-07-23 23:28:25 +0200

    panels functionality: closing panels, removing overlay

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const box3 = document.querySelector(".box-3");
 const moveUpBox1 = box1.querySelector(".content_move");
 const moveUpBox2 = box2.querySelector(".content_move");
 const moveUpBox3 = box3.querySelector(".content_move");
+const panels = document.querySelectorAll(".panel");
 const aboutPanel = document.getElementById("about");
 const resumePanel = document.getElementById("resume");
 const contactPanel = document.getElementById("contact");
@@ -17,6 +18,7 @@ const overlay = document.getElementById("overlay");
 const main = document.getElementById("main");
 const body = document.body;
 const viewMoreBtn = document.querySelectorAll(".view_more__btn");
+const panelCloseEl = document.querySelectorAll(".panel-close img");
 
 // FUNCTIONS
 
@@ -29,18 +31,72 @@ init();
 
 // Open panels
 
+// const openPanel = function () {
+//   panels.forEach(function(p) {
+//     p.classList.remove("hidden");
+//     p.classList.add("panel-move-left");
+//   })
+// };
+
 const openAboutPanel = function () {
   aboutPanel.classList.remove("hidden");
-  aboutPanel.classList.add("panel-move-left");
+  aboutPanel.classList.remove("panel-move-right");
+  const panelMoveLeft = function () {
+    aboutPanel.classList.add("panel-move-left");
+  };
+  setTimeout(panelMoveLeft, 100);
 };
 const openResumePanel = function () {
   resumePanel.classList.remove("hidden");
-  resumePanel.classList.add("panel-move-left");
+  resumePanel.classList.remove("panel-move-right");
+  const panelMoveLeft = function () {
+    resumePanel.classList.add("panel-move-left");
+  };
+  setTimeout(panelMoveLeft, 100);
 };
 
 const openContactPanel = function () {
   contactPanel.classList.remove("hidden");
-  contactPanel.classList.add("panel-move-left");
+  contactPanel.classList.remove("panel-move-right");
+  const panelMoveLeft = function () {
+    contactPanel.classList.add("panel-move-left");
+  };
+  setTimeout(panelMoveLeft, 100);
+};
+
+// Close panels
+
+const panelCloseElActive = function () {
+  panelCloseEl.forEach((p) => (p.style.opacity = "1"));
+};
+
+const panelCloseElPassive = function () {
+  panelCloseEl.forEach((p) => (p.style.opacity = "0.2"));
+};
+
+const closePanel = function () {
+  const hiddenPanel = function () {
+    panels.forEach((p) => p.classList.add("hidden"));
+  };
+  setTimeout(hiddenPanel, 600);
+  panels.forEach((p) => p.classList.remove("panel-move-left"));
+  panels.forEach((p) => p.classList.add("panel-move-right"));
+};
+
+const overlayDisactive = function () {
+  const removeOverlay = function () {
+    overlay.classList.remove("overlay--active");
+  };
+  setTimeout(removeOverlay, 600);
+  const moveLeftRem = function () {
+    main.classList.remove("move-left");
+  };
+  setTimeout(moveLeftRem, 100);
+  const moveRightAdd = function () {
+    main.classList.add("move-right");
+  };
+  setTimeout(moveRightAdd, 100);
+  body.classList.remove("overflow-hidden");
 };
 
 // Boxes content moving up and view more appearing
@@ -74,6 +130,7 @@ const moveDown = function (e) {
 
 const overlayActive = function () {
   overlay.classList.add("overlay--active");
+  main.classList.remove("move-right");
   main.classList.add("move-left");
   body.classList.add("overflow-hidden");
 };
@@ -91,3 +148,16 @@ boxes.addEventListener("click", overlayActive);
 
 boxes.addEventListener("mouseover", moveUp);
 boxes.addEventListener("mouseout", moveDown);
+
+// Closing panels
+
+panelCloseEl.forEach((el) =>
+  el.addEventListener("mouseover", panelCloseElActive)
+);
+panelCloseEl.forEach((el) =>
+  el.addEventListener("mouseleave", panelCloseElPassive)
+);
+
+panelCloseEl.forEach((el) => el.addEventListener("click", closePanel));
+
+panelCloseEl.forEach((el) => el.addEventListener("click", overlayDisactive));
