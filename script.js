a5f1a8b22fb8f4d43c5549d2d20a6d2f1b3e4dd0
Natalia Domanska <nataliaidomanska@gmail.com>
2023-08-10 16:40:57 +0200

    color design change: green

--- script.js
+++ script.js
@@ -110,7 +110,7 @@ const arrowDownClick = function () {
   boxes.scrollIntoView({ behavior: "smooth" });
   this.style.color = "#782347";
   setTimeout(() => {
-    this.style.color = "#2E0036";
+    this.style.color = "#044D29";
   }, 1000);
 };
 
@@ -198,7 +198,7 @@ arrowDown.addEventListener("mouseover", function () {
 });
 
 arrowDown.addEventListener("mouseleave", function () {
-  this.style.color = "#2E0036";
+  this.style.color = "#044D29";
 });
 
 arrowDown.addEventListener("click", arrowDownClick);
